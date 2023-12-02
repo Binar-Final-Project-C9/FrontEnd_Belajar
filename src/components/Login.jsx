06fa9d16b3b4ef2379b40c49982265898ddce3b3
Dika Wicaksono <dhkwck00@gmail.com>
2023-12-02 20:40:13 +0700

    add icon eye in input form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
+import { FaRegEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa";
 import appLogo from "../assets/Belajar_white 3.png";
-import { useState } from "react";
 
 const Login = () => {
   return (
@@ -20,7 +21,8 @@ const Login = () => {
           <div className="mb-5">
             <label
               htmlFor="username"
-              className="block text-sm font-medium text-gray-700">
+              className="block text-sm font-medium text-gray-700"
+            >
               Username
             </label>
             <input
@@ -30,33 +32,41 @@ const Login = () => {
               placeholder="Masukkan username"
             />
           </div>
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="/forgot-password"
-                  className="text-sm font-semibold text-indigo-600 hover:text-indigo-500">
-                  Lupa Kata Sandi
-                </a>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="text-sm">
+              <a
+                href="/forgot-password"
+                className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Lupa Kata Sandi
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-wrap relative rounded-2xl items-center">
+            <div className="relative w-full">
+              <input
+                type="password"
+                id="search"
+                className="flex-shrink flex-grow text-sm flex-1 w-full p-2 ps-4 border rounded-2xl"
+                placeholder="Masukkan Password"
+              />
+              <div className="absolute top-1/2 transform -translate-y-1/2 right-6 cursor-pointer">
+                <FaRegEye className="text-gray-600" />
               </div>
             </div>
-            <input
-              type="text"
-              id="password"
-              className="text-sm mt-1 w-full p-2 ps-4 border lg:w-[500px] rounded-2xl"
-              placeholder="Masukkan Password"
-            />
           </div>
           <Link to="/dashboard">
             <button
               href="/dashboard"
               className="bg-dark-blue text-white w-full font-normal text-sm h-[50px] mt-5 rounded-2xl"
-              type="button">
+              type="button"
+            >
               Masuk
             </button>
           </Link>
