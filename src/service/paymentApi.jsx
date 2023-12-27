07bf1412d64d77fdb9a0be375676af4a6f8489d1
Dika Wicaksono <dhkwck00@gmail.com>
2023-12-28 03:03:14 +0700

    add updatePayment in payment Api

--- src/service/paymentApi.jsx
+++ src/service/paymentApi.jsx
@@ -1,11 +1,21 @@
-import { apiSlice } from './api';
+import { apiSlice } from "./api";
 
 export const paymentApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     fetchPayment: builder.query({
-      query: () => '/payment',
+      query: () => "/payment",
+    }),
+    updatePaymentStatus: builder.mutation({
+      query: ({ id, updatedPayment }) => {
+        return {
+          url: `/payment/${id}`,
+          method: "PATCH",
+          body: updatedPayment,
+        };
+      },
     }),
   }),
 });
 
-export const { useFetchPaymentQuery } = paymentApi;
+export const { useFetchPaymentQuery, useUpdatePaymentStatusMutation } =
+  paymentApi;
